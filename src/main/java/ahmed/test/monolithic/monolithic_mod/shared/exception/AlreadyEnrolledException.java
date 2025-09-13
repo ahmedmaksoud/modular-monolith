6160d41cdb9fd72d4a029939d47afce9ee30d7cf
ahmedmaksoud <a.elmaksoud@gmail.com>
2025-09-13 11:21:43 +0400

    enhance exception syntax

--- src/main/java/ahmed/test/monolithic/monolithic_mod/shared/exception/AlreadyEnrolledException.java
+++ src/main/java/ahmed/test/monolithic/monolithic_mod/shared/exception/AlreadyEnrolledException.java
@@ -2,7 +2,7 @@ package ahmed.test.monolithic.monolithic_mod.shared.exception;
 
 public class AlreadyEnrolledException extends RuntimeException {
     public AlreadyEnrolledException(String description) {
-        super("Not Found: " + (description != null ? description : ""));
+        super("Error: " + (description != null ? description : ""));
 
     }
 }
