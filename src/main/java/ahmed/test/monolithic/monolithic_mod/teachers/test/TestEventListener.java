c1a67cf7bd351df193617638e4ca2941accdd189
ahmedmaksoud <a.elmaksoud@gmail.com>
2025-09-13 11:56:12 +0400

    test rollback event listener

--- src/main/java/ahmed/test/monolithic/monolithic_mod/teachers/test/TestEventListener.java
+++ src/main/java/ahmed/test/monolithic/monolithic_mod/teachers/test/TestEventListener.java
@@ -1,24 +1,30 @@
-package ahmed.test.monolithic.monolithic_mod.subjects.internal.infrastructure.db;
+package ahmed.test.monolithic.monolithic_mod.teachers.test;
 
 import ahmed.test.monolithic.monolithic_mod.shared.IApplogger;
 import ahmed.test.monolithic.monolithic_mod.shared.infrastructure.AppLoggers;
-import ahmed.test.monolithic.monolithic_mod.students.internal.domain.model.Student;
 import ahmed.test.monolithic.monolithic_mod.students.shared.events.StudentRegistered;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Service
-class ServTest {
+public class TestEventListener {
     private final ApplicationEventPublisher events;
-    IApplogger iApplogger = AppLoggers.get(ServTest.class);
+    IApplogger iApplogger = AppLoggers.get(TestEventListener.class);
 
-    ServTest(ApplicationEventPublisher events) {
+    TestEventListener(ApplicationEventPublisher events) {
+
         this.events = events;
     }
-    @ApplicationModuleListener
+
+    //test roll back if some exception happen
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void test1(StudentRegistered studentRegistered) {
         iApplogger.info("test1 listener " + studentRegistered);
+       // throw new RuntimeException("dddddddddddddddddddddddddddddddddddddddd");
+
     }
     @ApplicationModuleListener
     public void test2(StudentRegistered studentRegistered) {
