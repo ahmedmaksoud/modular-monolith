8b0371b7abf8ae95bf6bb43e20626ba4168b2e15
ahmedmaksoud <a.elmaksoud@gmail.com>
2025-09-17 21:15:42 +0400

    disable exception

--- src/main/java/ahmed/test/monolithic/monolithic_mod/shared/infrastructure/GenericExceptionHandler.java
+++ src/main/java/ahmed/test/monolithic/monolithic_mod/shared/infrastructure/GenericExceptionHandler.java
@@ -32,40 +32,40 @@ public class GenericExceptionHandler {
         this.messageSource = messageSource;
     }
 
-    @ExceptionHandler({ AppException.class})
-    // @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<ErrorResponse> handle(Exception e, WebRequest request) {
-        AppException ex = null;
-        //UserDTO userDTO = jwtUtils.getUserInfoFromJwtToken(context);
-        log.debug("ServiceException  1 " + e.getMessage());
-        if (e instanceof AppException) {
-            ex = (AppException) e;
-        }
-       // ex.printStackTrace();
-        ErrorResponse errorResponse = new ErrorResponse();
-        Optional<List<AppMessage>> errorDTOs = ex.getErrorCodes();
-        List<MessageDetails> errors = new ArrayList<MessageDetails>();
-        Locale loc = getReqLocale(request);
-        if (errorDTOs != null)
-            errors = errorDTOs.get().stream()
-                    .map(element -> new MessageDetails(
-                            getMessageResource(element.getMessageCode(), loc,
-                                    element.getParams()
-                            ),
-                            element.getMessageType(), element.getFieldName(), element.getLinkUrl(),
-                            element.getMessageCode()))
-                    .collect(Collectors.toList());
-        if (errors.isEmpty())
-            errors.add(new MessageDetails(getMessageResource("general.exception", loc, null) + "  " +ex.getMessage(),
-                    AppMessage.MessageTypes.ERROR, "", "", ""));
-        errorResponse.setMainErrorCode("APP_BR_10000");
-        errorResponse.setMessage(ex.getExceptionMessage());
-        errorResponse.setErrors(errors);
-        errorResponse.setStatus(HttpStatus.BAD_REQUEST);
-        errorResponse.setExtraData("{}");
-        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
-
-    }
+//    @ExceptionHandler({ AppException.class})
+//    // @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public ResponseEntity<ErrorResponse> handle(Exception e, WebRequest request) {
+//        AppException ex = null;
+//        //UserDTO userDTO = jwtUtils.getUserInfoFromJwtToken(context);
+//        log.debug("ServiceException  1 " + e.getMessage());
+//        if (e instanceof AppException) {
+//            ex = (AppException) e;
+//        }
+//       // ex.printStackTrace();
+//        ErrorResponse errorResponse = new ErrorResponse();
+//        Optional<List<AppMessage>> errorDTOs = ex.getErrorCodes();
+//        List<MessageDetails> errors = new ArrayList<MessageDetails>();
+//        Locale loc = getReqLocale(request);
+//        if (errorDTOs != null)
+//            errors = errorDTOs.get().stream()
+//                    .map(element -> new MessageDetails(
+//                            getMessageResource(element.getMessageCode(), loc,
+//                                    element.getParams()
+//                            ),
+//                            element.getMessageType(), element.getFieldName(), element.getLinkUrl(),
+//                            element.getMessageCode()))
+//                    .collect(Collectors.toList());
+//        if (errors.isEmpty())
+//            errors.add(new MessageDetails(getMessageResource("general.exception", loc, null) + "  " +ex.getMessage(),
+//                    AppMessage.MessageTypes.ERROR, "", "", ""));
+//        errorResponse.setMainErrorCode("APP_BR_10000");
+//        errorResponse.setMessage(ex.getExceptionMessage());
+//        errorResponse.setErrors(errors);
+//        errorResponse.setStatus(HttpStatus.BAD_REQUEST);
+//        errorResponse.setExtraData("{}");
+//        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
+//
+//    }
     /**
      * This method replace params in massage with values from DTO
      * @param params
